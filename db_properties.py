337989e5222501942b51a9194b0c2f1be978608a
anirudhaUN <anirudha.un@thoughtclan.com>
2023-03-02 11:56:30 +0530

    collection names updated

--- db_properties.py
+++ db_properties.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 
 cluster=MongoClient("mongodb+srv://anirudhaun:cY5hdsdwvg1aHTex@cluster0.suvfptn.mongodb.net/?retryWrites=true&w=majority")
 db=cluster["Newsfeed"]
-source_collection=db["Collection"]
-rssfeed_collection=db["rssCollection"]
+source_collection=db["source"]
+rssfeed_collection=db["rssData"]
