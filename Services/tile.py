75cb2b3112db23350bfc54ad79c53589b32ab0e0
keerthis897 <124707792+keerthis897@users.noreply.github.com>
2023-03-05 12:50:52 +0530

    Update tile.py

--- Services/tile.py
+++ Services/tile.py
@@ -1,6 +1,15 @@
 from db_properties import *
 from mongo_utils import *
 
-get_categories()
-get_article_details()
+def get_category():
+    category=get_categories()
+    return category
+
+def get_article():
+    article_details = get_article_details()
+    articles = []
+    for article in article_details:
+        articles.append(article)
+    return articles
+  
 get_relative_links()
