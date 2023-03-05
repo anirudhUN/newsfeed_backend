7a29a26f69e4590a231f4af23646c0e75a18d015
keerthis897 <124707792+keerthis897@users.noreply.github.com>
2023-03-05 11:23:22 +0530

    article methods

--- Services/article.py
+++ Services/article.py
@@ -1,22 +1,7 @@
-import pymongo
-from pymongo import MongoClient
 from db_properties import *
 from mongo_utils import *
-import time
 
-def fetch_article():
-    articles = source_collection.find({}).sort('LastAccessTime', pymongo.DESCENDING)
-    return articles
-
-while True:
-    articles = fetch_article()
-    if articles:
-        print(articles['content'])
-    else:
-        time.sleep(1200)  
-
-
-def get_tag_news(tag):
-      results=source_collection.find({"Tags":tag},{})
-      for i in results:
-            print(i)
+def get_article_details():
+def get_tag_news():
+def relative_links():
+   
