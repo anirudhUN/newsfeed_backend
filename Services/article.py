2562e5a4791714bd6b4c9f68e87887380bade2a5
NAMRATHA141 <126678070+NAMRATHA141@users.noreply.github.com>
2023-03-07 11:47:53 +0530

    update article

--- Services/article.py
+++ Services/article.py
@@ -4,17 +4,8 @@ sys.path.insert(0, '/path/to/other/folder') # path from newsfeed_backend to serv
 from db_properties import *
 from mongo_utils import *
 
-def get_article():
-    article_details = get_article_details()
-    articles = []
-    for article in article_details:
-        articles.append(article)
-    return articles
- 
-def get_tag():
-    tag=get_tag_news()
-    return tag
-
-def relative_links(id):
-    related_documents = get_relative_links(id)
-    return related_documents
+def init_article():
+    articles = list(get_article_details())
+    tag = list(get_tag_news())
+    relative_links = list(get_relative_links(article[_id]))
+    return({'tag' : tag, 'articles':articles,'relative_links': relative_links})
