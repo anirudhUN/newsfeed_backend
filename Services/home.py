e8a813ff22040924284263ebc8ba1af990a8a984
NAMRATHA141 <126678070+NAMRATHA141@users.noreply.github.com>
2023-03-07 11:30:07 +0530

    Update home.py

--- Services/home.py
+++ Services/home.py
@@ -13,3 +13,10 @@ def update_home_page(last_page_count):
     page_to_fetch = last_page_count + 1
     articles = get_successive_articles(SOURCE_COLLECTION,PAGE_COUNT,page_to_fetch)
     return list(articles)
+
+def category_page(user_cat):
+    categories = get_categories()
+    if user_cat in categories:
+         cat_news = get_cat_news(user_cat)
+    
+    return {"category": user_cat, "articles": cat_news}
