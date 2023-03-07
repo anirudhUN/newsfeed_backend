ce650c6283982617d20a4ca1849428cd970fe3fa
keerthis897 <124707792+keerthis897@users.noreply.github.com>
2023-03-07 11:50:55 +0530

    Update tile.py

--- Services/tile.py
+++ Services/tile.py
@@ -4,21 +4,9 @@ sys.path.insert(0, '/path/to/other/folder') # path from newsfeed_backend to serv
 from db_properties import *
 from mongo_utils import *
 
-def get_category():
-    category=get_categories()
-    return category
-
-def get_article():
-    article_details = get_article_details()
-    articles = []
-    for article in article_details:
-        articles.append(article)
-    return articles
-
-def get_tag():
-    tag=get_tag_news()
-    return tag
-
-def relative_links(id):
-    related_documents = get_relative_links(id)
-    return related_documents
+def init_article():
+    categories = get_categories()
+    articles = list(get_article_details())
+    tag = list(get_tag_news())
+    relative_links = list(get_relative_links(article[_id]))
+    return({'categories':categories, 'articles':articles, 'tag' : tag, 'relative_links': relative_links})
