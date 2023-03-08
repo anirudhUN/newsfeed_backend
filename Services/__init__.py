3eb5b24d301b8998d73b2c607f979c76973ca387
keerthis897 <124707792+keerthis897@users.noreply.github.com>
2023-03-08 10:32:08 +0530

    Update __init__.py

--- Services/__init__.py
+++ Services/__init__.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, 'newsfeed_backend/Services') # path from newsfeed_backend to services folder, path can change so not specified. 
+sys.path.insert(0, 'newsfeed_backend/services') # path from newsfeed_backend to services folder, path can change so not specified. 
 
 from db_properties import *
 from mongo_utils import *
