1531f303248dd1e5447f0082f07f5999bfd3b27b
keerthi <keerthis897@gmail.com>
2023-03-09 18:30:26 +0530

    article updated

--- Services/article.py
+++ Services/article.py
@@ -2,13 +2,12 @@ from properties.db_properties import *
 from utils.mongo_utils import *
 
 def init_article():
-    article = get_article_details(source_collection, limit=1)[0]
+    article_id = 'article_id'
+    article = get_article_details(article_id)
     tags = article['tags']
+
     related_articles = set()
     for tag in tags:
-
-    
-
         related_articles = source_collection.find({'tags': tag})
         for related_article in related_articles:
             if related_article['_id'] != article['_id'] and related_article not in related_articles:
