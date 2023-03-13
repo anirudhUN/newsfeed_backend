19f128e07ed620c28016adce3f14f8c266e77bbc
keerthi <keerthis897@gmail.com>
2023-03-13 09:15:40 +0530

    utils updated

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -93,4 +93,26 @@ def related_links(collection_name,article_id):
     links=collection_name.find({"category":category},{"title":1,"link":1})
     return list(links)
 
+def get_related_articles(article_id):
+    article = get_article_details(article_id)
+    tags = article['tags']
+    category = article['category']
+    related_articles = []
+
+    for tag in tags:
+        tag_related_articles = source_collection.find({'tags': tag, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
+        for related_article in tag_related_articles:
+            if related_article not in related_articles:
+                related_articles.append(related_article)
+                break
+
+    for cat in category:
+        cat_related_articles = source_collection.find({'category': cat, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
+        for related_article in cat_related_articles:
+            if related_article not in related_articles:
+                related_articles.append(related_article)
+                break
+
+    return related_articles
+
 
