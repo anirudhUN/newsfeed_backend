acb99862558cf3e534c6ef19e9c8c75460327564
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-14 19:50:12 +0530

    script.py updated

--- utils/script.py
+++ utils/script.py
@@ -3,14 +3,15 @@ import pandas as pd
 import json
 import datetime
 from pymongo import MongoClient
+from properties.db_properties import *
 
 client = MongoClient("mongodb+srv://anirudhaun:cY5hdsdwvg1aHTex@cluster0.suvfptn.mongodb.net/?retryWrites=true&w=majority")
-db = client['Newsfeed']
-collection = db['rssData']
+db = client[DATABASE_NAME]
+collection = db[RSS_FEED_COLLECTION]
 
 #collection.delete_many({})
 
-df=pd.read_csv("C:\\Users\\Dhanush Gowda\\Downloads\\Data Model and Functional Requirements - RSSfeed.csv")
+df=pd.read_csv("git_re\\newsfeed_backend\\data\\RSSfeed_details.csv")
 data=df.to_dict(orient="records")
 #collection.insert_many(data)
 
