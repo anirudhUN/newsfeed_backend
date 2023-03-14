1f054feaa5968a4cdd8c070a854f2682f0d2cfb1
anirudhaUN <anirudha.un@thoughtclan.com>
2023-03-14 16:19:22 +0530

    collection updated

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -13,9 +13,8 @@ from datetime import datetime
 cluster=MongoClient("mongodb+srv://anirudhaun:cY5hdsdwvg1aHTex@cluster0.suvfptn.mongodb.net/?retryWrites=true&w=majority")
 
 database=cluster[DATABASE_NAME]
-source_collection=database[SOURCE_COLLECTION]
 rssfeed_collection=database[RSS_FEED_COLLECTION]
-trial_collection=database[TRIAL_SOURCE]
+article_collection=database[ARTICLE_COLLECTION]
 
 
 #to insert directly using rss feed url
@@ -35,12 +34,12 @@ def insert_rss_doc(collection,url, FIELD_MAP):
         collection.insert_one(doc)
 
 
-def insert_one(document):
-    result = source_collection.insert_one(document)
+def insert_one(collection,document):
+    result = collection.insert_one(document)
     return result
 
-def insert_many(documents):
-    result = source_collection.insert_many(documents)
+def insert_many(collection,documents):
+    result = collection.insert_many(documents)
     return result
 
 def get_categories(collection_name):
@@ -86,8 +85,8 @@ def update_document(collection_name,query,update):
     result = collection_name.update_one(query,update)
     return result
     
-def get_article_details(article_id):
-    article = source_collection.find_one({'_id': ObjectId(article_id)})
+def get_article_details(collection,article_id):
+    article = collection.find_one({'_id': ObjectId(article_id)})
     if article is None:
         print(f"No article found with ID {article_id}")
         return None
@@ -99,21 +98,21 @@ def related_links(collection_name,article_id):
     links=collection_name.find({"category":category},{"title":1,"link":1})
     return list(links)
 
-def get_related_articles(article_id):
+def get_related_articles(collection,article_id):
     article = get_article_details(article_id)
     tags = article['tags']
     category = article['category']
     related_articles = []
 
     for tag in tags:
-        tag_related_articles = source_collection.find({'tags': tag, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
+        tag_related_articles = collection.find({'tags': tag, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
         for related_article in tag_related_articles:
             if related_article not in related_articles:
                 related_articles.append(related_article)
                 break
 
     for cat in category:
-        cat_related_articles = source_collection.find({'category': cat, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
+        cat_related_articles = collection.find({'category': cat, '_id': {'$ne': article['_id']}}, projection={'_id': 0, 'title': 1, 'description': 1})
         for related_article in cat_related_articles:
             if related_article not in related_articles:
                 related_articles.append(related_article)
@@ -123,4 +122,4 @@ def get_related_articles(article_id):
 
 
 if __name__=="__main__":
-    insert_rss_doc(trial_collection,"https://mobilesyrup.com/feed/",FIELD_MAP)
+    insert_rss_doc(article_collection,"https://mobilesyrup.com/feed/",FIELD_MAP)
