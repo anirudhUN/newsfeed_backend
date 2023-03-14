c5627901a71fe4fef1c80ffaae6ce3cbc1f845b6
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-14 16:18:18 +0530

    script.py updated

--- utils/script.py
+++ utils/script.py
@@ -2,18 +2,23 @@ import pymongo
 import pandas as pd
 import json
 import datetime
-from mongo_utils import *
+from pymongo import MongoClient
 
-df=pd.read_csv("Data Model and Functional Requirements - Sheet5.csv")
+client = MongoClient("mongodb+srv://anirudhaun:cY5hdsdwvg1aHTex@cluster0.suvfptn.mongodb.net/?retryWrites=true&w=majority")
+db = client['Newsfeed']
+collection = db['rssData']
+
+#collection.delete_many({})
+
+df=pd.read_csv("C:\\Users\\Dhanush Gowda\\Downloads\\Data Model and Functional Requirements - RSSfeed.csv")
 data=df.to_dict(orient="records")
 #collection.insert_many(data)
-def update_rss(collection):
-    collection.insert_many(data)
+
+current_time = datetime.datetime.now()
+for document in collection.find():
     current_time = datetime.datetime.now()
-    for document in collection.find():
-        current_time = datetime.datetime.now()
-        collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
-        
-    collection.update_many({}, {'$set': {'Status': "True"}})
+    collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
     
-    print(f"{collection.count_documents({})} documents updated.")
+collection.update_many({}, {'$set': {'Status': "True"}})
+
+print(f"{collection.count_documents({})} documents updated.")
