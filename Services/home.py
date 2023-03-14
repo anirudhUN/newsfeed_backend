c90d84d9eea0fa37e587877251107664c6dfb223
anirudhaUN <anirudha.un@thoughtclan.com>
2023-03-14 10:52:26 +0530

    path updated

--- Services/home.py
+++ Services/home.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
 
