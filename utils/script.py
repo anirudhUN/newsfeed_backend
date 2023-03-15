8847ec355eedeca53abb40925eb0938b8bbe9494
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-15 13:42:45 +0530

    script updated

--- utils/script.py
+++ utils/script.py
@@ -1,25 +1,24 @@
-import pymongo
+import sys
+import os
 import pandas as pd
-import json
-import datetime
-from pymongo import MongoClient
+from datetime import datetime
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 from properties.db_properties import *
+from utils.mongo_utils import *
 
-client = MongoClient("mongodb+srv://anirudhaun:cY5hdsdwvg1aHTex@cluster0.suvfptn.mongodb.net/?retryWrites=true&w=majority")
-db = client[DATABASE_NAME]
-collection = db[RSS_FEED_COLLECTION]
-
-#collection.delete_many({})
+#rssfeed_collection.delete_many({})
 
 df=pd.read_csv("git_re\\newsfeed_backend\\data\\RSSfeed_details.csv")
 data=df.to_dict(orient="records")
-#collection.insert_many(data)
+#rssfeed_collection.insert_many(data)
 
-current_time = datetime.datetime.now()
-for document in collection.find():
-    current_time = datetime.datetime.now()
-    collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
+current_time = datetime.now()
+for document in rssfeed_collection.find():
+    current_time = datetime.now()
+    rssfeed_collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
     
-collection.update_many({}, {'$set': {'Status': "True"}})
+rssfeed_collection.update_many({}, {'$set': {'Status': "True"}})
 
-#print(f"{collection.count_documents({})} documents updated.")
+#print(f"{rssfeed_collection.count_documents({})} documents updated.")
