4d89084236e3c029a294c21840d984b5cc5e0a88
keerthi <keerthis897@gmail.com>
2023-03-15 15:35:01 +0530

    category service updated

--- Services/category.py
+++ Services/category.py
@@ -6,13 +6,13 @@ sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
 
-def init_category_page(user_cat):
+def retrieve_articles_for_category(user_cat):
     categories = get_categories(article_collection)
     if user_cat in categories:
-        cat_news = get_cat_news(article_collection,user_cat)
+        articles = get_cat_news(article_collection,user_cat)
     else:
-        cat_news = []  
-    return {"category": user_cat, "articles": cat_news}
+        articles = []  
+    return {"category": user_cat, "articles": articles}
 
 def generate_category_list(article_collection):
     categories = get_categories(article_collection)
