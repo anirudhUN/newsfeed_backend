1ef4aac035cedb0c56a4fc4f58be43495c991583
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-16 14:05:25 +0530

    rssdata_insertion updated

--- utils/rssdata_insertion.py
+++ utils/rssdata_insertion.py
@@ -8,16 +8,15 @@ sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
 
-#rssfeed_collection.delete_many({})
+rssfeed_collection.delete_many({})
 
 df=pd.read_csv("git_re\\newsfeed_backend\\data\\RSSfeed_details.csv")
 data=df.to_dict(orient="records")
-if rssfeed_collection.count_documents({}) == 0:
-    rssfeed_collection.insert_many(data)
-
-else:
+#if rssfeed_collection.count_documents({}) == 0:
+rssfeed_collection.insert_many(data)
+current_time = datetime.now()
+for document in rssfeed_collection.find():
     current_time = datetime.now()
-    for document in rssfeed_collection.find():
-        current_time = datetime.now()
-        rssfeed_collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
-    rssfeed_collection.update_many({}, {'$set': {'Status': "True"}})
+    rssfeed_collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
+rssfeed_collection.update_many({}, {'$set': {'Status': "True"}})
+
