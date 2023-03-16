3fc34558ffaa7befc259068ac41f99a0cfbd053e
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-16 11:52:40 +0530

    rssdata updated

--- utils/rssdata_insertion.py
+++ utils/rssdata_insertion.py
@@ -20,4 +20,4 @@ else:
     for document in rssfeed_collection.find():
         current_time = datetime.now()
         rssfeed_collection.update_one({'_id': document['_id']}, {'$set': {'last_access_time': current_time}})
-    rssfeed_collection.update_many({}, {'$set': {'Status': "True"}}
+    rssfeed_collection.update_many({}, {'$set': {'Status': "True"}})
