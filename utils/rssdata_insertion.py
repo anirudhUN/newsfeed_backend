14002a87fa392fc34ede42266b4995d8b3804db4
gowda-pavan <pavangowda2001123@gmail.com>
2023-03-16 14:07:53 +0530

    rssdata_insertion upadted

--- utils/rssdata_insertion.py
+++ utils/rssdata_insertion.py
@@ -12,7 +12,6 @@ rssfeed_collection.delete_many({})
 
 df=pd.read_csv("git_re\\newsfeed_backend\\data\\RSSfeed_details.csv")
 data=df.to_dict(orient="records")
-#if rssfeed_collection.count_documents({}) == 0:
 rssfeed_collection.insert_many(data)
 current_time = datetime.now()
 for document in rssfeed_collection.find():
