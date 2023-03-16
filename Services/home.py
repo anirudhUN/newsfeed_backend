007ab7e4e5a7b6a4b641bd759a844f22bb7d5ee5
keerthi <keerthis897@gmail.com>
2023-03-16 14:40:17 +0530

    home service updated

--- Services/home.py
+++ Services/home.py
@@ -6,11 +6,9 @@ sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
 
-def init_home_page(article_collection):
+def init_home_page(page=None):
+    if page is None:
+        page = 1
     categories = get_categories(article_collection)
-    articles = list(get_latest_articles(article_collection))
+    articles = get_successive_articles(article_collection,page)
     return {'categories':categories, 'articles':articles}
-
-def update_home_page(article_collection,page):
-    articles = get_successive_articles(article_collection, page)
-    return list(articles)
