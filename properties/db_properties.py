31c44807669926cbae6197f3c81c3f15f620898a
keerthi <keerthis897@gmail.com>
2023-03-27 11:04:31 +0530

    SOURCE_SELECTOR_MAPPER updated

--- properties/db_properties.py
+++ properties/db_properties.py
@@ -16,15 +16,47 @@ FIELD_MAP = {
 PATH = "..\chromedriver.exe"
 
 SOURCE_SELECTOR_MAPPER={
-             "Techcrunch" : "div.article-content",
-             "Tech republic": "section.article-body",
-             "Tech Talksick":"div.text",
-             "Firstpost Tech":"div.text-content-wrap",
-             "Hackaday":"div.entry-content",
-             "MobileSyrup": "div.article-content",
-             "Droid Life":"div.entry-content__post",
-             "Droidviews":"div.entry-content"
-        }
+"Tech republic": {
+   'article': "section.article-body",
+   'tags' : 'section.categories ul li a',
+   'image' : 'section.article-body figure img'
+},
+ "Techcrunch": {
+    'article': 'div.article-content',
+    'tags': 'div.article__tags h3 + ul li a',
+    'image' : 'div.article__content-wrap img'
+},
+"Tech Talksick" :{
+    'article': 'div.text',
+    'tags': 'div.bottom-tags p a',
+    'image' : 'div.epcl-loader img'
+},
+"Firstpost Tech": {
+    "article" : "div.text-content-wrap",
+    'tags' : "div.article__tags ul li a",
+    'image' : 'figure.wp-caption img'
+},
+"Hackaday":{
+    'article' : "div.entry-content",
+    'tags' : "span.tags-links a",
+    'image' : "div.entry-featured-image img"
+},
+"MobileSyrup": {
+    'article' : "div.article-content",
+    'tags' : "",
+    'image' : "div.main.has-sidebar img"
+},
+"Droid Life": {
+    'article' : "h1.entry-title",
+    'tags' : "div.entry-taxonomy entry-taxonomies__category ul li a",
+    'image' : "div.picture.picture__post-featured img"
+},
+"Droidviews": {
+    'article' : "h1.entry-title",
+    'tags' : "",
+    'image' : "div.entry-content p img"
+}
+}
 
 #categories
 APPS="apps"
