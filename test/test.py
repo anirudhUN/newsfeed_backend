e22c59434daeb30a5fa4f54e9e30c7fd0f66bc6f
keerthi <keerthis897@gmail.com>
2023-04-04 12:02:22 +0530

    updated

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
-from Services.contentScraping import *
+from services.content_scraping import *
 
 #get_cat_news(trial_collection,APPS)
 
