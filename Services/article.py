9ed28ed70073f6e3cf91f3dbb3cb0c99e359e5ac
gowda-pavan <pavangowda2001123@gmail.com>
2023-04-04 14:02:42 +0530

    related_articles.py and article.py services updated

--- Services/article.py
+++ Services/article.py
@@ -5,7 +5,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
-from related_articles import *
+from services.related_articles import *
 
 def init_article(article_id):
     article = get_article_details(article_id)
--- Services/related_articles.py
+++ Services/related_articles.py
@@ -5,6 +5,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from properties.db_properties import *
 from utils.mongo_utils import *
+from services import *
 
 def get_related_articles(article_id):
     article = get_article_details(article_id)
