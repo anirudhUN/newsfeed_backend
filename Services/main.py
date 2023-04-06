5cadc6f9a2bc2d323275f698172896a9f9f6d41e
anirudhUN <anirudha.un@thoughtclan.com>
2023-04-06 11:01:07 +0530

    main.py updated

--- Services/main.py
+++ Services/main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
 from services.rss_services import process_rss_feeds
 
 if __name__ == '__main__':
