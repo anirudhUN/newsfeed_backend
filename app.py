b1a27450592c5788eb9a38a438da61db3cfc64fa
anirudhUN <anirudha.un@thoughtclan.com>
2023-04-15 13:48:43 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from api.api import bp as api_bp
 
 app = Flask(__name__)
+CORS(app, origins='http://localhost:3000', methods=['GET', 'POST'], allow_headers=['Content-Type'])
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+ 
